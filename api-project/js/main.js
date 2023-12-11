46595720aa46a5e5b13eb663cb55055d75dabb51
jaylaye <yejayla69@gmail.com>
2023-12-11 09:29:17 -0500

    .

--- api-project/js/main.js
+++ api-project/js/main.js
@@ -1,18 +1,9 @@
-import '../styles/style.css'
+import '../styles/style.css';
 
-const url = "https://pokeapi.co/api/v2/pokemon/";
-async function getData(URL){
-    try{
+const URL = "https://pokeapi.co/api/v2/pokemon/"
+async function getData(URL) {
     const response = await fetch(URL);
-    if (response.status !=200){
-        throw new Error(response.statusText);
-    }
-    console.log(response);
     const data = await response.json();
     console.log(data);
-    } catch (error) {
-        console.log("error");
-        document.querySelector("api").textContent = "error";
-    }
 }
 getData(URL);
